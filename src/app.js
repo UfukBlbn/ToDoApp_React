53b2f0d04c602986e2ea5eee5ee0f49c7fbeffd5
Ufuk Blbn <97101197+UfukBalaban@users.noreply.github.com>
2022-04-21 09:35:13 +0300

    firstCommit

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import TodoApp from './components/TodoApp';
+import './styles/main.css';
+
+ReactDOM.render(<TodoApp />, document.getElementById('root'));
